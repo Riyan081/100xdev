0474f08abe7fc90253f488653757ded967146733
Riyan081 <waterdlaw810@gmail.com>
2026-03-01 01:29:50 +0530

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "sans-serif" }}>
       <h1>Hello World</h1>
-      <p>Welcome to my Next.js app hi i am messi h</p>
+      <p>Welcome to my Next.js app hi i am messi hi</p>
     </div>
   );
 }
